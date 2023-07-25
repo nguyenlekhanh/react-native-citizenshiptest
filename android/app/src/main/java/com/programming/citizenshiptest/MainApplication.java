02dc42b56ec98a46feec53f860cf40dfa6a6e491
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-07-25 13:29:27 -0500

    rename app

--- android/app/src/main/java/com/programming/citizenshiptest/MainApplication.java
+++ android/app/src/main/java/com/programming/citizenshiptest/MainApplication.java
@@ -1,4 +1,4 @@
-package com.firstinit;
+package com.programming.citizenshiptest;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/citizenshiptestTests/citizenshiptestTests.m
+++ ios/citizenshiptestTests/citizenshiptestTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface firstInitTests : XCTestCase
+@interface citizenshiptestTests : XCTestCase
 
 @end
 
-@implementation firstInitTests
+@implementation citizenshiptestTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
