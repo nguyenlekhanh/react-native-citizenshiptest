893cd3115b4719a41d52336faebc7cd4c88f5422
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-07-25 12:22:56 -0500

    combine slice with zustand

--- App.tsx
+++ App.tsx
@@ -14,16 +14,19 @@ import {
 } from 'react-native-heroicons/outline';
 
 import { useBearStore } from './app/store.zustand';
+import { useBoundStore } from './app/store.zustandCombine';
 
 function App(): JSX.Element {
   const bears = useBearStore((state) => state.bears);
   const increaseBears = useBearStore((state) => state.increase);
 
+  const bears1 = useBoundStore((state) => state.bears);
+  const addBear = useBoundStore((state) => state.addBear);
   return (
     <SafeAreaView>
       <View className="flex-row items-center space-x-2 pb-2 mx-4">
-        <Text>Bears: {bears}</Text>
-        <Button onPress={() => increaseBears(1)} title="Increase Bear"/>
+        <Text>Bears: {bears1}</Text>
+        <Button onPress={() => addBear(1)} title="Add Bears"/>
         <ChevronDownIcon size={20} color="#00FFBB"/>
       </View>
     </SafeAreaView>
