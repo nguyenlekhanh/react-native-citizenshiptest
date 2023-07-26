73458dd27f3e1229241ebfb088f0e73bfc15d5e9
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-07-25 22:29:13 -0500

    add google ads

--- screens/AdsScreen.tsx
+++ screens/AdsScreen.tsx
@@ -1,11 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+
+const adUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-7627591966956528/9929092494';
 
 const AdsScreen: React.FC = () => {
   return (
-    <View>
-      <Text>Ads</Text>
-    </View>
+    <BannerAd
+      unitId={adUnitId}
+      size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+      requestOptions={{
+        requestNonPersonalizedAdsOnly: true,
+      }}
+    />
   )
 }
 
