80245a1b39dacb381741d8471f469e15a5b9d027
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-07-25 21:53:54 -0500

    homescreen style

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,6 +4,8 @@ import Ads from './AdsScreen';
 import { TouchableOpacity } from 'react-native';
 
 const HomeScreen: React.FC = () => {
+  const buttonStyle = "w-auto p-3 bg-lime-500 rounded bold mt-2";
+
   return (
     <SafeAreaView className="flex-columns items-center w-full h-max">
         <View className="w-full h-[90%] mt-2">
@@ -12,8 +14,11 @@ const HomeScreen: React.FC = () => {
                 <Text className="bold text-2xl">2008</Text>
               </View>
               <View>
-                  <TouchableOpacity className="w-auto p-3 bg-lime-500 rounded bold bold">
-                      <Text className="text-[#fff] text-xl">100 questions 2008 Civics Test</Text>
+                  <TouchableOpacity className={buttonStyle}>
+                      <Text className="text-[#fff] text-xl">View 100 questions 2008 Civics Test</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity className={buttonStyle}>
+                      <Text className="text-[#fff] text-xl">Test 100 questions 2008 Civics Test</Text>
                   </TouchableOpacity>
               </View>
 
@@ -21,8 +26,11 @@ const HomeScreen: React.FC = () => {
                 <Text className="bold text-2xl">2020</Text>
               </View>
               <View>
-                  <TouchableOpacity className="w-auto p-3 bg-lime-500 rounded bold bold">
-                      <Text className="text-[#fff] text-xl">128 questions 2020 Civics Test</Text>
+                  <TouchableOpacity className={buttonStyle}>
+                      <Text className="text-[#fff] text-xl">View 128 questions 2020 Civics Test</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity className={buttonStyle}>
+                      <Text className="text-[#fff] text-xl">Test 128 questions 2020 Civics Test</Text>
                   </TouchableOpacity>
               </View>
             </View>
